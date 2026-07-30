05ef7e8f6dbc74c68f66d2b30028b538f8f69a18
HarikaPenukonda <penukondaharika@gmail.com>
2026-07-30 14:49:09 -0500

    redirecting reponses to html file

--- src/main/java/FirstServletApp.java
+++ src/main/java/FirstServletApp.java
@@ -7,15 +7,19 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-/**
- * Servlet implementation class FirstServletApp
- */
+
 @WebServlet("/firstServlet")
 public class FirstServletApp extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	public void doPost(HttpServletRequest request, HttpServletResponse response) 
 											throws ServletException, IOException {
+		
+		
+		String username = request.getParameter("fullname");
+		String password = request.getParameter("password");
+		
+		response.sendRedirect("register-success.html");
 		
 	}
 
